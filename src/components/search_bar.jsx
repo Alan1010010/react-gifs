8e5c2b8a8164195793af1da4cc2496308d62f6f5
Alan Quinn <alancquinn2@gmail.com>
2020-07-20 18:03:28 +0100

    add autofocus, fix css

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+// componentDidMount(){
+//    this.input.focus();
+// }
+
   handleChange = (e) => {
     this.props.searchFunction(e.target.value);
   }
 
   render() {
     return (
-        <input type="text" className="form-control form-search" onChange={this.handleChange}/>
+        <input autoFocus="true" type="text" className="form-control form-search" onChange={this.handleChange}/>
       )
   }
 }
